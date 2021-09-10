d5a82409a2652ecf865126495faf52cd433f6725
Grant M. Seiter <seiterecon@gmail.com>
2021-09-10 14:32:24 -0400

    Build v0.1.0 corrections

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="app",
     description="Local installation of app.py file.",
-    url="https://github.com/grantseiter/MNC-Profit-Shifting-App",
+    url="https://github.com/grantseiter/OECD-Corporate-Tax-Burden-App",
     packages=setuptools.find_packages(),
     include_package_data=True,
 )
